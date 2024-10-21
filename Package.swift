22c4c66764476e60afe65922de9523e044f9763f
Alexey <avgx@users.noreply.github.com>
2024-10-21 13:05:49 +0300

    Update Package.swift
    fix link for binary

--- Package.swift
+++ Package.swift
@@ -1,13 +1,13 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
-let vlcBinary = Target.binaryTarget(name: "VLCKit-all", url: "https://github.com/tylerjonesio/vlckit-spm/releases/download/v3.6.0.b10/VLCKit-all.xcframework.zip", checksum: "c17bb7d14d2f603a225d879f22e673f488d3c67145b9fafe02ed8dfa80273601")
+let vlcBinary = Target.binaryTarget(name: "VLCKit-all", url: "https://github.com/avgx/test-vlc/releases/download/2024-10-21/VLCKit-all.xcframework.zip", checksum: "7e448ebb132e204d62f3b0db488dc3a1bb1c42ced34c16b4275ef7c6971e3348")
 
 let package = Package(
     name: "vlckit-spm",
-    platforms: [.macOS(.v10_13), .iOS(.v11), .tvOS(.v11)],
+    platforms: [.iOS(.v15), .tvOS(.v15)],
     products: [
         .library(
             name: "VLCKitSPM",
@@ -35,7 +35,6 @@ let package = Package(
                 .linkedLibrary("xml2", .when(platforms: [.iOS, .tvOS])),
                 .linkedLibrary("z", .when(platforms: [.iOS, .tvOS])),
                 .linkedLibrary("bz2", .when(platforms: [.iOS, .tvOS])),
-                .linkedFramework("Foundation", .when(platforms: [.macOS])),
                 .linkedLibrary("iconv")
             ]),
     ]
